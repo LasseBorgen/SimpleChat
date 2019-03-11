322fbb10b43583c6aa47292f2622c4e3038f471c
LasseBorgen <00alasse@gmail.com>
2019-03-11 10:13:39 +0100

    few comments

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,13 @@
 const functions = require('firebase-functions')
 
+//TODO---Opdateringer burde nok ske gennem HTTP-requests i stedet for direkte at tilgå databasen
+//Lige nu sker opdateringen -> brugerne får besked gennem deres snapshot -> Den her trigger opdaterer igen ->
+//Brugerne får besked for 2. gang
 exports.createPost = functions.firestore.document('posts/{postId}').onCreate((snap, context) => {
-    const post = snap.data();
-    console.log(post)
     content = snap.data().content
-    const regExp = /Microsoft|Google|Apple/i
-    content = content.replace(regExp, '********')
+    content = content.replace(/Microsoft|Google|Apple/gi, '********')
     
     return snap.ref.set({
         content: content
-    }, { merge: true });
-
-    
+    }, { merge: true });  
 })
